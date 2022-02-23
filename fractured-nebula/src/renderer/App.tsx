faed74db4b541d3bb0b2ed698ffff63430cfaf37
Marijn Bitter <marijn@goeiezaak.com>
2022-02-23 17:29:13 +0100

    Fixed GIT

---
+++ fractured-nebula/src/renderer/App.tsx
@@ -0,0 +1,20 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+const Hello = () => {
+  return (
+    <div>
+      <p>this is a test</p>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
